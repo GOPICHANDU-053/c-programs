52b6fec49bd805b6b337363f70be3f127a1263ba
VAKA GOPI CHANDU <vaakagopichand@gmail.com>
2026-04-22 15:42:23 +0530

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+  int a;
+  printf("enter A value \n");
+  scanf("%d",&a);
+  printf("A is not a positive value i.e is:%d",a<0);
+    return 0;
+}
